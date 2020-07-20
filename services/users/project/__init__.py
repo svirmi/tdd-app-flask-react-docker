69a97e40351c348d7f1da2201fe4522e3ee49742
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2020-07-20 08:59:45 +0300

    reading config settings

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,9 +1,17 @@
+import os
+import sys
 from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 # set config
-app.config.from_object('project.config.DevelopmentConfig')
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
+
+# print(app.config, file=sys.stderr)
+# to see the config in logs
+# docker-compose -f docker-compose-dev.yml logs
+
 
 @app.route('/users/ping', methods=['GET'])
 def ping_pong():
