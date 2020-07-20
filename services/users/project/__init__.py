06b7f5be337b4ff11f0d1b68f898e86a7eea5121
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2020-07-20 09:10:27 +0300

    setting SQLAlchemy

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,6 +1,8 @@
 import os
-import sys
+import datetime
+# import sys
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
@@ -11,6 +13,10 @@ app.config.from_object(app_settings)
 # print(app.config, file=sys.stderr)
 # to see the config in logs
 # docker-compose -f docker-compose-dev.yml logs
+
+# instantiate the db
+db = SQLAlchemy(app)
+
 
 
 @app.route('/users/ping', methods=['GET'])
