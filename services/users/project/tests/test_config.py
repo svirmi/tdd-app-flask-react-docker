8dc29b95e287737f9e2c8e037fb77ad6410cd86a
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2020-07-22 09:09:29 +0300

    testingConfig test and secret key added

--- services/users/project/tests/test_config.py
+++ services/users/project/tests/test_config.py
@@ -18,6 +18,19 @@ class TestDevelopmentConfig(TestCase):
             os.environ.get('DATABASE_URL')
         )
 
+class TestTestingConfig(TestCase):
+    def create_app(self):
+        app.config.from_object('project.config.TestingConfig')
+        return app
+    def test_app_is_testing(self):
+        self.assertTrue(app.config['SECRET_KEY'] == 'my_precious')
+        self.assertTrue(app.config['TESTING'])
+        self.assertFalse(app.config['PRESERVE_CONTEXT_ON_EXCEPTION'])
+        self.assertTrue(
+            app.config['SQLALCHEMY_DATABASE_URI'] ==
+            os.environ.get('DATABASE_TEST_URL')
+        )
+
 class TestProductionConfig(TestCase):
     def create_app(self):
         app.config.from_object('project.config.ProductionConfig')
